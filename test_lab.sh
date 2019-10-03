f16d4b268a7a8e18e30959e84ed47184cde6a7a4
Ivan Baksheev <zpix-dev@list.ru>
2019-10-03 16:03:56 +0700

    test_lab.sh added to test labs

---
+++ test_lab.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+# set -ex;
+
+# Configuration
+# $1 - lab dir (ex. lab0, lab8-0, ...)
+# Please edit these lines
+# Wine path
+WINE="/Applications/Wine Stable.app/Contents/Resources/wine/bin/wine"
+# Compile N Run script, you can download it here (TODO: add link)
+COMPILE_N_RUN="/Users/john/saves/tester/compile_n_run.py"
+# Default test directory (/lab0/$DEFAULT_CNR_TESTS_DIR/*.test_file)
+DEFAULT_CNR_TESTS_DIR="my_test"
+# A server to compile code, also edit curl if you want to change compilation method
+WINE_COMPILE_URL="http://188.225.38.69:7777/api.php" 
+
+RESULT=0
+
+cd $1;
+
+# My tests (compile_n_run)
+echo "compile_n_run tests:"
+
+for test_file in $DEFAULT_CNR_TESTS_DIR/*.test_file 
+do
+    $COMPILE_N_RUN src/main.c -t $test_file;
+    let "RESULT += $?"
+done;
+
+# Wine testing (Petrov's tester)
+echo "Wine tests:"
+echo "compilation..."
+CODE=`printf "%s" | cat src/main.c`
+# Compile and get exe url
+URL=`curl -s $WINE_COMPILE_URL --data-urlencode "code=$CODE" --data "comp_command=c"`
+# Download exe
+wget $URL -q -O test/main.exe
+if [[ $? != 0 ]]; then echo "Problem downloading exe file" && exit $?; fi
+
+cd test
+"$WINE" test*.exe main.exe 2> /dev/null
+let "RESULT += $?"
+cd ..
+
+if [[ $RESULT == 0 ]];
+then 
+    echo "SUCCESS. ALL TESTS PASSED!";
+else
+    echo "ERROR! SOME TESTS FAILED: RESULT_CODE=$RESULT"
+fi;
